e29382c55b057d11de6fff9ed87433511686bb52
ErikOlsen <erik.s.olsen@gmail.com>
2014-06-03 11:15:05 -0500

    second test plus refactoring of test 1

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -5,16 +5,26 @@ describe Task do
   let (:task) {Task.new('Run', 'Marathon', 'Active')}
 
   context '#initialize' do
-    it "creates a new Task object with three arguments" do
+
+    it "should have title" do
       expect(task.title).to eq('Run')
+    end
+    it "should have description" do
       expect(task.description).to eq('Marathon')
+    end
+    it "should have a status" do
       expect(task.status).to eq('Active')
-      # expect(task.created_at).to eq(Time.now)
+    end
+    it "should have a created_at" do
+      # mock time.now to always return the same time
+      now = Time.now
+      expect(Time).to receive(:now).and_return(now)
+      expect(task.created_at).to eq(now)
     end
 
     it "Upon initializing the default status should be 'incomplete'" do
       incomplete_task = Task.new('Laundry', 'Clean up dirty clothes')
-      expect(incomplete_task.status).to be('incomplete')
+      expect(incomplete_task.status).to eq('incomplete')
     end
 
   end #context#initialize-end
--- todo.rb
+++ todo.rb
@@ -1,11 +1,13 @@
+require 'byebug'
+
 class Task
   attr_reader :title, :description, :status
-  # attr_accessor :created_at
-  def initialize(title, description, status)
+  attr_accessor :created_at
+  def initialize(title, description, status='incomplete')
     @title = title
     @description = description
     @status = status
-    # @created_at = Time.now
+    @created_at = Time.now
   end
 
 end
