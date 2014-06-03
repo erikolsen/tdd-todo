df2a5b7cbfeb31dd2c5692104a8e161245467f7a
ErikOlsen <erik.s.olsen@gmail.com>
2014-06-03 10:52:08 -0500

    first test minus created_at

--- spec/spec_pizza.rb
+++ spec/spec_pizza.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe Pizza do
-  let(:pizza) {Pizza.new}
+  let(:pizza) {Pizza.new("cheese", "Gooey Cheese")}
   context "#initialize" do
     it "creates a Pizza object" do
       expect(pizza).to be_an_instance_of Pizza
     end
-    xit "has a name" do
+    it "has a name" do
       expect(pizza.name).to eq "cheese"
     end
-    xit "has a description" do
+    it "has a description" do
       expect(pizza.description).to eq "Gooey Cheese"
     end
-    xit "has a time_baked" do
-      expect(pizza.time_baked).to eq 12
+    it "has a time_baked default of 0" do
+      expect(pizza.time_baked).to eq 0
     end
   end
+
+  context '#set_time'
 end
--- todo.rb
+++ todo.rb
@@ -1,9 +1,11 @@
 class Task
+  attr_reader :title, :description, :status
+  # attr_accessor :created_at
   def initialize(title, description, status)
     @title = title
     @description = description
     @status = status
+    # @created_at = Time.now
   end
-end
 
-puts Task.attributes
+end
