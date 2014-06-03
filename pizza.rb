6b9503e2b5a3ddc26b5454695f13688d573a7e65
Oli Treadwell & Parth Naik <pair+olitreadwell.parthnaik@devbootcamp.com>
2014-06-03 11:00:34 -0500

    completed all current pizza test specs, modified created_at Time.now test spec, added next To Do list test spec

--- pizza.rb
+++ pizza.rb
@@ -1,4 +1,10 @@
 class Pizza
+  attr_reader :name, :description, :time_baked
+
   def initialize
+    @name = 'cheese'
+    @description = 'Gooey Cheese'
+    @time_baked = 12
   end
 end
+
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -5,15 +5,15 @@ describe Task do
 
   context '#initialize' do
     it "creates a new Task object with three arguments" do
-      expect(:task.title).to be('Run')
-      expect(:task.description).to be('Marathon')
-      expect(:task.status).to be('Active')
-      expect(:task.created_at).to be(Time.now)
+      expect(task.title).to eq('Run')
+      expect(task.description).to eq('Marathon')
+      expect(task.status).to eq('Active')
+      expect(task.created_at.to_s).to eq(Time.now.to_s)
     end
 
     it "Upon initializing the default status should be 'incomplete'" do
       incomplete_task = Task.new('Laundry', 'Clean up dirty clothes')
-      expect(incomplete_task.status).to be('incomplete')
+      expect(incomplete_task.status).to eq('incomplete')
     end
 
   end #context#initialize-end
